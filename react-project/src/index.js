87487a900aa1773a216ec8b92400db82df91d85a
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-06-25 14:49:23 +0300

    What is JSX

--- react-project/src/index.js
+++ react-project/src/index.js
@@ -4,11 +4,24 @@ import ReactDom from 'react-dom';
 // function App(){
 //     return <h1>Hello guys!</h1>
 // }
+const name ="murat"
+const surname="çayır"
 
-class App2 extends React.Component {
-    render(){
-        return <h1>How are you?</h1>
-    }
+// const names =["murat","ömer","taha"]
+
+function App(){
+return(
+    <div>
+        {/* {names.map(name=>(<h1>{name}</h1>))}
+        <ul>
+            <li>murat</li>
+            <li>murat</li>
+        </ul> */}
+
+       
+
+        <button type="button" style={{padding:'5px',backgroundColor:'yellowgreen',border:'3px solid dark'}} >{name.concat(surname)}</button>
+    </div>
+)
 }
-
-ReactDom.render(<App2/>,document.getElementById('root'));
+ReactDom.render(<App/>,document.getElementById('root'));
