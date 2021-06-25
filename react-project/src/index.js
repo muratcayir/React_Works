e397cd7f9b8c8d8dc69dfe2e00a9928b4692f5d0
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-06-25 16:03:11 +0300

    props concepts

--- react-project/src/index.js
+++ react-project/src/index.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-// function App(){
-//     return <h1>Hello guys!</h1>
-// }
-const name ="murat"
-const surname="çayır"
 
-// const names =["murat","ömer","taha"]
+import Card from './components/Card'
 
-function App(){
+
+
+const App =()=>{
+
 return(
-    <div>
-        {/* {names.map(name=>(<h1>{name}</h1>))}
-        <ul>
-            <li>murat</li>
-            <li>murat</li>
-        </ul> */}
+<div>
+      <div className="card-group">
 
-       
+         <Card  cardTitle="first card"   />
 
-        <button type="button" style={{padding:'5px',backgroundColor:'yellowgreen',border:'3px solid dark'}} >{name.concat(surname)}</button>
-    </div>
+         <Card  cardTitle="second card"/> 
+
+         <Card  cardTitle="third card"/> 
+         
+      </div>
+</div>
 )
 }
 ReactDom.render(<App/>,document.getElementById('root'));
