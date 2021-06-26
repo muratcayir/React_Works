c4b53e406f70641226beaab9dc91ddd387aeaca3
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-06-26 15:39:02 +0300

    functional and class components

--- react-project/src/components/Card.js
+++ react-project/src/components/Card.js
@@ -1,16 +1,24 @@
-const Card = (props) => {
+import React from 'react';
+
+class Card extends React.Component {
+  render() {
     return (
-        <div className="card">
-          <img src={props.image} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">{props.cardTitle}</h5>
-              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                <p className="card-text">{props.cardText}</p>
-                <p className="card-text"><small className="text-muted">{props.cardUpdated}</small></p>
-            </div>
-          </div>
+      <div className="card w-100">
+        <img src={this.props.image} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{this.props.cardTitle}</h5>
+          <p className="card-text"></p>
+          <p className="card-text">
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </p>
+          <p className="card-text">{this.props.cardText}</p>
+          <p className="card-text">
+            <small className="text-muted">{this.props.cardUpdated}</small>
+          </p>
+        </div>
+      </div>
     );
-};
+  }
+}
 
 export default Card;
--- react-project/src/index.js
+++ react-project/src/index.js
@@ -5,33 +5,42 @@ import Collapse from './components/Collapse';
 
 const App = () => {
   return (
-    <div>
-      <div className="card-group">
-        
-          <Card
-          cardTitle="first card"
-          cardText="lorem ipsum "
-          cardUpdated=" 5 minutes ago"
-          image="https://picsum.photos/id/1/200/300"
-        />
-       
+    <div className="container">
+      <div className="row">
+        <div className="card-group w-100">
+          <div className="col">
+            <Collapse href="collapExample1">
+              <Card
+                cardTitle="first card"
+                cardText="lorem ipsum "
+                cardUpdated=" 5 minutes ago"
+                image="https://picsum.photos/id/1/200/300"
+              />
+            </Collapse>
+          </div>
 
-          <Collapse href="collapseExample2">
-          <Card
-          cardTitle="second card"
-          cardText="lorem ipsum "
-          cardUpdated=" 5 minutes ago"
-          image="https://picsum.photos/id/1000/200/300"
-        />
-          </Collapse>
+          <div className="col">
+            <Collapse href="collapExample2">
+              <Card
+                cardTitle="first card"
+                cardText="lorem ipsum "
+                cardUpdated=" 5 minutes ago"
+                image="https://picsum.photos/id/10/200/300"
+              />
+            </Collapse>
+          </div>
 
-          <Collapse href="collapseExample3">
-         No card
-          </Collapse>
-      
-
-       
-        
+          <div className="col">
+            <Collapse href="collapExample3">
+              <Card
+                cardTitle="first card"
+                cardText="lorem ipsum "
+                cardUpdated=" 5 minutes ago"
+                image="https://picsum.photos/id/100/200/300"
+              />
+            </Collapse>
+          </div>
+        </div>
       </div>
     </div>
   );
