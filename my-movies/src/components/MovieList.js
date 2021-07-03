1aa51520fb8033e67b9c173218583d91e469c246
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-07-03 15:18:30 +0300

    Delete component

---
+++ my-movies/src/components/MovieList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+ const MovieList = (props) =>  {
+
+    //  function handleClick (){
+
+    //  }
+  
+
+        return  (
+            <div className="row">
+
+                {props.movies.map((movie)=>(
+                    <div className="col-lg-4">
+                    <div className="card mb-4 shadow-sm" key={movie.id}>
+                        <img src={movie.imageURL} className="card-img-top" alt="Sample Movie" />
+                        <div className="card-body">
+                            <h5 className={movie.name}>Sample Movie</h5>
+                            <p className={movie.overview}>Sample Movie Description</p>
+                            <div className="d-flex justify-content-between align-items-center">
+                                <button type="button" onClick={(event)=>props.deleteMovieProp(movie)} className="btn btn-md btn-outline-danger">Delete</button>
+                                <h2><span className="badge badge-info">{movie.rating}</span></h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                ))}
+
+            </div>
+        )
+
+    
+}
+
+
+
+export default MovieList
