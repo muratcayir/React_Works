9173ec4854ed1830d80aba14843e35790213c765
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-07-17 20:58:04 +0300

    swagger is added

--- my-movies/src/components/SearchBar.js
+++ my-movies/src/components/SearchBar.js
@@ -1,31 +1,37 @@
-import React from "react";
+import React from 'react';
 
 class SearchBar extends React.Component {
-  state = {
-    search: "",
-  };
 
-  handleFormSubmit = (event) => {
-    event.preventDefault();
-  };
 
-  render() {
-    return (
-      <form onSubmit={this.handleFormSubmit}>
-        <div className="form-row- mb-5">
-          <div className="col-12">
-            <input
-              onChange={this.props.searchMovieProp}
-              type="text"
-              className="form-control"
-              placeholder="Search a movie"
-              value={this.setState.value}
-            />
-          </div>
-        </div>
-      </form>
-    );
-  }
+    handleFormSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    render() {
+
+        return  (
+            <form onSubmit={this.handleFormSubmit}>
+                <div className="form-row mb-5">
+                    <div className="col-10">
+                        <input 
+                        
+                            onChange={this.props.searchMovieProp} 
+                            type="text" className="form-control" 
+                            placeholder="Seach a movie" 
+                        />
+                    </div>
+                    <div className="col-2">
+                        <button type="button" 
+                                className="btn btn-md btn-danger"
+                                style={{float:'right'}}>Add Movie
+                        </button>
+                    </div>
+                </div>
+            </form>
+        )
+
+    }
 }
+
 
 export default SearchBar;
