bda67407769e788ffc04ad096a1270d528a1264e
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-07-29 16:30:58 +0300

    new project is added

---
+++ books/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BookList from './BookList';
+
+class App extends React.Component {
+
+    render () {
+
+        return <div><BookList /></div>
+    }
+}
+
+export default App;
---
+++ books/src/components/BookList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './BookList.css';
+import Book from './Book';
+import BookContext from '../contexts/BookContext';
+
+class BookList extends React.Component {
+
+    static contextType = BookContext;
+
+    render() {
+
+        const books = this.context
+
+        return (
+            <section className="page-section bg-light" id="portfolio">
+                <div className="container">
+                    <div className="text-center">
+                        <h2 className="section-heading text-uppercase">BookFolio</h2>
+                        <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    </div>
+                    <div className="row">
+
+                        {books.map((book, i) => {
+                            return <Book book={book}
+                                key={i}
+                            />
+                        })}
+
+                    </div>
+                </div>
+            </section>
+        )
+    }
+
+}
+
+export default BookList;
