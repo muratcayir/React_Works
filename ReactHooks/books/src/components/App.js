799852cf26e178f5ebb593f9c042a4a56a4fdd83
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-07-30 22:18:27 +0300

    Provider and Consumer

--- ReactHooks/books/src/components/App.js
+++ ReactHooks/books/src/components/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import BookList from './BookList';
+import BookContextProvider from '../contexts/BookContext'
 
 class App extends React.Component {
 
     render () {
 
-        return <div><BookList /></div>
-    }
+        return <div>
+                <BookContextProvider>
+                    <BookList />
+                </BookContextProvider>
+              </div>
+              }
 }
 
 export default App;
--- ReactHooks/books/src/contexts/BookContext.js
+++ ReactHooks/books/src/contexts/BookContext.js
@@ -1,7 +1,11 @@
 import React from "react"
 
-export default  React.createContext(
 
+export const BookContext = React.createContext()
+
+class BookContextProvider extends React.Component{
+  state ={
+    books:
     [
         {      
           title: "Kırmızı Pazartesi",
@@ -44,8 +48,21 @@ export default  React.createContext(
           pageNum: 296,
           imageURL: "https://i.idefix.com/cache/600x600-0/originals/0001780787001-1.jpg",
           topic: "1968-1970 yılları arasında geçen olaylar, o günün toplumsal gerçeklerini de satırlara taşıyor. Ama romanın odağında bu toplumsal olaylar değil üçlü bir aşk var. Gençliğin rüzgârıyla hareketlenen İmkânsızın Şarkısını ölümle erken karşılaşan gençlerin hayatı yönlendiriyor. Hiçbir şeyin önem taşımadığı, amaçsızlığın ağır bastığı, özgür seksin kol gezdiği bir öğrenci hayatı... Ama diğer yanda da yoğun duygular var... İmkânsız aşklar, imkânsız şarkılar söyleten. Hemen hemen her Japon gencinin okuduğu roman anayurdu dışında da çok kişi tarafından sahipleniliyor."
+        }],
+      }
+        render(){
+          return(
+            <BookContext.Provider value={this.state}>
+             {this.props.children}
+            </BookContext.Provider>
+          )
         }
-    
-        ]
+        
+  }
 
-)
+
+
+
+//console.log(Mycontext)
+
+export default BookContextProvider;
