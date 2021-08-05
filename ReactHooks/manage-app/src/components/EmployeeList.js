b5e11738cc8eba618714a49978277c061688c602
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-08-05 16:53:20 +0300

    Context ,Consumer , Provider

--- ReactHooks/manage-app/src/components/EmployeeList.js
+++ ReactHooks/manage-app/src/components/EmployeeList.js
@@ -1,36 +1,68 @@
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Employee from './Employee';
+import { Button, Modal } from 'react-bootstrap';
+import { EmployeeContext } from '../contexts/EmployeeContext';
+import AddForm from './AddForm';
 
-const Employeelist = () => {
+const EmployeeList = () => {
 
-const [employees, setEmployees] = useState([
-        {id:1, name: 'Thomas Hardy', email: 'thomashardy@mail.com', address: '89 Chiaroscuro Rd, Portland, USA', phone: '(171) 555-2222'},
-        {id:2, name: 'Dominique Perrier', email: 'dominiqueperrier@mail.com', address: 'Obere Str. 57, Berlin, Germany', phone: '(313) 555-5735'},
-        {id:3, name: 'Maria Anders', email: 'mariaanders@mail.com', address: '25, rue Lauriston, Paris, France', phone: '(503) 555-9931'},
-        {id:4, name: 'Fran Wilson', email: 'franwilson@mail.com', address: 'C/ Araquil, 67, Madrid, Spain', phone: '(204) 619-5731'},
-        {id:5, name: 'Martin Blank', email: 'martinblank@mail.com', address: 'Via Monte Bianco 34, Turin, Italy', phone: '(480) 631-2097'}
-])
+    const {employees} = useContext(EmployeeContext)
 
+    const [show, setShow] = useState(false)
 
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true)
 
-  return (
-   
-      <table className="table table-striped table-hover">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Address</th>
-            <th>Phone</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          <Employee employees={employees}/>
-        </tbody>
-      </table>
-   
-  );
-};
+    useEffect(() => {
+        handleClose();
+    }, [employees])
 
-export default Employeelist;
+    return (
+
+        <>
+        <div className="table-title">
+        <div className="row">
+          <div className="col-sm-6">
+            <h2>Manage <b>Employees</b></h2>
+          </div>
+          <div className="col-sm-6">
+            <Button onClick={handleShow} className="btn btn-success text-white" data-toggle="modal"><i className="material-icons">&#xE147;</i> <span>Add New Employee</span></Button>
+          </div>
+        </div>
+      </div>
+
+        <table className="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Address</th>
+                    <th>Phone</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <Employee employees={employees}/>
+            </tbody>
+        </table>
+
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header className="modal-header" closeButton>
+                <Modal.Title>
+                    Add Employee
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <AddForm />
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close Modal
+                </Button>
+            </Modal.Footer>
+        </Modal>
+        </>
+    )
+}
+
+export default EmployeeList;
