c7fb9e5af23603d32da17d9c0fbc32436d719536
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-08-05 13:49:13 +0300

    useState

--- ReactHooks/manage-app/src/App.js
+++ ReactHooks/manage-app/src/App.js
@@ -2,8 +2,34 @@ import Employeelist from './components/EmployeeList';
 
 function App() {
   return (
-    <div className="App">
-     <Employeelist/>
+    <div>
+      <div className="container-xl">
+        <div className="table-responsive">
+          <div className="table-wrapper">
+            <div className="table-title">
+              <div className="row">
+                <div className="col-sm-6">
+                  <h2>
+                    Manage <b>Employees</b>
+                  </h2>
+                </div>
+                <div className="col-sm-6">
+                  <a
+                    href="#addEmployeeModal"
+                    className="btn btn-success"
+                    data-toggle="modal"
+                  >
+                    <i className="material-icons">&#xE147;</i>{' '}
+                    <span>Add New Employee</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <Employeelist />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- ReactHooks/manage-app/src/components/Employee.js
+++ ReactHooks/manage-app/src/components/Employee.js
@@ -1,13 +1,24 @@
-import Employeelist from "./EmployeeList"
-
-const  Employee = ()=> {
-    return(
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    )
-}
+const Employee = ({ employees }) => {
+  return (
+    <>
+      {employees.map((employee) => (
+        <tr key={employee.id}>
+          <td>{employee.name}</td>
+          <td>{employee.email}</td>
+          <td>{employee.adress}</td>
+          <td>{employee.phone}</td>
+          <td>
+            <a href="#editEmployeeModal" className="edit" data-toggle="modal">
+              <i className="material-icons" data-toggle="tooltip" title="Edit"> &#xE254;</i>
+            </a>
+            <a href="#deleteEmployeeModal" className="delete" data-toggle="modal">
+              <i className="material-icons" data-toggle="tooltip"  title="Delete">&#xE872;</i>
+            </a>
+          </td>
+        </tr>
+      ))}
+    </>
+  );
+};
 
 export default Employee;
