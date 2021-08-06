844a310907f66d7dc3edd1acdab4541f9aa703fa
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-08-06 12:16:52 +0300

    AddForm

--- ReactHooks/manage-app/src/components/AddForm.js
+++ ReactHooks/manage-app/src/components/AddForm.js
@@ -1,38 +1,82 @@
-import {Form, Button} from 'react-bootstrap'
+import { Form, Button } from 'react-bootstrap';
+import { EmployeeContext } from '../contexts/EmployeeContext';
+import { useContext, useState } from 'react';
 
-const AddForm = ()=>{
+const AddForm = () => {
 
-    return(
-        <Form>
+    const { dispatch } = useContext(EmployeeContext);
+
+/*     const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [address, setAddress] = useState("");
+    const [phone, setPhone] = useState(""); */
+
+    const [newEmployee, setNewEmployee] = useState({
+        name:"", email:"", address: "", phone: ""
+    })
+
+    const {name, email, address, phone } = newEmployee;
+
+    const onInputChange = (e) => {
+        setNewEmployee({...newEmployee, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //addEmployee(name, email, address, phone)
+        dispatch({type: 'add_employee', employee : {
+            name, email, address, phone
+        }})
+    }
+
+
+    return (
+        <Form onSubmit={handleSubmit}>
+
             <Form.Group>
                 <Form.Control
-                type="text"
-                placeholder="Name *"
-                required
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Control
-                type="email"
-                placeholder="Email *"
-                required
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Control
-                as="textarea"
-                placeholder="Adress *"
-                rows={3}
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Control
-                type="text"
-                placeholder="Phone *"
+                    type="text"
+                    placeholder="Name *"
+                    name="name"
+                    value={name}
+                    onChange={e => onInputChange(e)}
+                    required 
                 />
             </Form.Group>
 
-            <Button variant="succes" type="submit" block>
+            <Form.Group>
+                <Form.Control
+                    type="email"
+                    placeholder="Email *"
+                    name="email"
+                    value={email}
+                    onChange={e => onInputChange(e)}
+                    required 
+                />
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Control
+                    as="textarea"
+                    placeholder="Address *"
+                    name="address"
+                    value={address}
+                    onChange={e => onInputChange(e)}
+                    rows={3} 
+                />
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Control
+                    type="text"
+                    placeholder="Phone"
+                    name="phone"
+                    value={phone}
+                    onChange={e => onInputChange(e)}
+                />
+            </Form.Group>
+
+            <Button variant="success" type="submit" block>
                 Add New Employee
             </Button>
         </Form>
