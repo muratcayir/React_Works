228389a9d20397ce88b9a291daf532093ec7a447
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-08-07 18:00:52 +0300

    Alert Notification

--- ReactHooks/manage-app/src/components/EmployeeList.js
+++ ReactHooks/manage-app/src/components/EmployeeList.js
@@ -1,74 +1,97 @@
 import { useContext, useEffect, useState } from 'react';
 import Employee from './Employee';
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Modal, Alert } from 'react-bootstrap';
 import { EmployeeContext } from '../contexts/EmployeeContext';
 import AddForm from './AddForm';
 
 const EmployeeList = () => {
+  const { employees } = useContext(EmployeeContext);
 
-    const {employees} = useContext(EmployeeContext)
+  const [show, setShow] = useState(false);
 
-    const [show, setShow] = useState(false)
+  const [showAlert, setShowAlert] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true)
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  //   const handleShowAlert = () =>setShowAlert(true)
 
-    useEffect(() => {
-        handleClose();
-    }, [employees])
+  const handleShowAlert = () => {
+    setShowAlert(true);
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 5000);
+  };
 
-    return (
+  useEffect(() => {
+    handleClose();
 
-        <>
-        <div className="table-title">
+    return handleShowAlert();
+  }, [employees]);
+
+  return (
+    <>
+      <div className="table-title">
         <div className="row">
           <div className="col-sm-6">
-            <h2>Manage <b>Employees</b></h2>
+            <h2>
+              Manage <b>Employees</b>
+            </h2>
           </div>
           <div className="col-sm-6">
-            <Button onClick={handleShow} className="btn btn-success text-white" data-toggle="modal"><i className="material-icons">&#xE147;</i> <span>Add New Employee</span></Button>
+            <Button
+              onClick={handleShow}
+              className="btn btn-success text-white"
+              data-toggle="modal"
+            >
+              <i className="material-icons">&#xE147;</i>{' '}
+              <span>Add New Employee</span>
+            </Button>
           </div>
         </div>
       </div>
 
-        <table className="table table-striped table-hover">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Address</th>
-                    <th>Phone</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-               {
-employees.map((employee) =>(
-    <tr key={employee.id}>
-<Employee employee={employee}/>
-    </tr>
-))
-               }
-            </tbody>
-        </table>
+      <Alert show={showAlert} variant="success" dismissible>
+        Employee List successfuly updated!
+      </Alert>
 
-        <Modal show={show} onHide={handleClose}>
-            <Modal.Header className="modal-header" closeButton>
-                <Modal.Title>
-                    Add Employee
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <AddForm />
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                    Close Modal
-                </Button>
-            </Modal.Footer>
-        </Modal>
-        </>
-    )
-}
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Address</th>
+            <th>Phone</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees
+            .sort((a, b) => (a.name < b.name ? -1 : 1))
+            .map((employee) => (
+              <tr key={employee.id}>
+                <Employee employee={employee} />
+              </tr>
+            ))}
+        </tbody>
+      </table>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header className="modal-header" closeButton>
+          <Modal.Title>Add Employee</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <AddForm />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close Modal
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
 
 export default EmployeeList;
+
+//sort((a,b) => a.name.localeCompare(b.name))
