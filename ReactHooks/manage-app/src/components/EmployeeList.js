f283531cfb1f0f67cf436baf85ca14440416cbb6
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-08-08 15:07:25 +0300

    pagination

--- ReactHooks/manage-app/src/components/EmployeeList.js
+++ ReactHooks/manage-app/src/components/EmployeeList.js
@@ -1,97 +1,109 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState} from 'react';
 import Employee from './Employee';
 import { Button, Modal, Alert } from 'react-bootstrap';
 import { EmployeeContext } from '../contexts/EmployeeContext';
 import AddForm from './AddForm';
+import Pagination from './Pagination';
 
 const EmployeeList = () => {
-  const { employees } = useContext(EmployeeContext);
 
-  const [show, setShow] = useState(false);
+    const {sortedEmployees} = useContext(EmployeeContext)
 
-  const [showAlert, setShowAlert] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
+    const [show, setShow] = useState(false)
+    const [currentPage, setCurrentPage] = useState(1);
+    const [employeesPerPage] = useState(2)
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  //   const handleShowAlert = () =>setShowAlert(true)
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true)
+    //const handleShowAlert = () => setShowAlert(true);
 
-  const handleShowAlert = () => {
-    setShowAlert(true);
-    setTimeout(() => {
-      setShowAlert(false);
-    }, 5000);
-  };
+    const handleShowAlert = () => {
+        setShowAlert(true);
+        setTimeout(()=> {
+            setShowAlert(false);
+        }, 2000);
+    };
 
-  useEffect(() => {
-    handleClose();
+    useEffect(() => {
+        handleClose();
 
-    return handleShowAlert();
-  }, [employees]);
+        return () => {
+            handleShowAlert();
+        }
+    }, [sortedEmployees])
 
-  return (
-    <>
-      <div className="table-title">
+    const indexOfLastEmployee = currentPage * employeesPerPage;
+    const indexOfFirstEmployee = indexOfLastEmployee - employeesPerPage;
+    const currentEmployees = sortedEmployees.slice(indexOfFirstEmployee, indexOfLastEmployee);
+    const totalPagesNum = Math.ceil(sortedEmployees.length /employeesPerPage)
+
+
+    return (
+
+        <>
+        <div className="table-title">
         <div className="row">
           <div className="col-sm-6">
-            <h2>
-              Manage <b>Employees</b>
-            </h2>
+            <h2>Manage <b>Employees</b></h2>
           </div>
           <div className="col-sm-6">
-            <Button
-              onClick={handleShow}
-              className="btn btn-success text-white"
-              data-toggle="modal"
-            >
-              <i className="material-icons">&#xE147;</i>{' '}
-              <span>Add New Employee</span>
-            </Button>
+            <Button onClick={handleShow} className="btn btn-success text-white" data-toggle="modal"><i className="material-icons">&#xE147;</i> <span>Add New Employee</span></Button>
           </div>
         </div>
       </div>
 
-      <Alert show={showAlert} variant="success" dismissible>
-        Employee List successfuly updated!
+      <Alert show={showAlert} variant="success">
+        Employee List successfully updated!.
       </Alert>
 
-      <table className="table table-striped table-hover">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Address</th>
-            <th>Phone</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {employees
-            .sort((a, b) => (a.name < b.name ? -1 : 1))
-            .map((employee) => (
-              <tr key={employee.id}>
-                <Employee employee={employee} />
-              </tr>
-            ))}
-        </tbody>
-      </table>
+        <table className="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Address</th>
+                    <th>Phone</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    currentEmployees.map((employee) =>(
+                        <tr key={employee.id}>
+                            <Employee employee={employee} />
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </table>
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header className="modal-header" closeButton>
-          <Modal.Title>Add Employee</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <AddForm />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close Modal
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
-};
+        <Pagination 
+            pages = {totalPagesNum} 
+            setCurrentPage  =   {setCurrentPage}
+            currentEmployees = {currentEmployees} 
+            sortedEmployees = {sortedEmployees}
+            />
+
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header className="modal-header" closeButton>
+                <Modal.Title>
+                    Add Employee
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <AddForm />
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close Modal
+                </Button>
+            </Modal.Footer>
+        </Modal>
+
+        
+        </>
+    )
+}
 
 export default EmployeeList;
-
-//sort((a,b) => a.name.localeCompare(b.name))
--- ReactHooks/manage-app/src/contexts/EmployeeContext.js
+++ ReactHooks/manage-app/src/contexts/EmployeeContext.js
@@ -1,4 +1,3 @@
-  
 import { createContext, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -6,7 +5,7 @@ export const EmployeeContext = createContext();
 
 const EmployeeContextProvider = (props) => {
 
-    const [employees,setEmployees] = useState([
+    const [employees, setEmployees] = useState([
         {id:uuidv4(), name: 'Thomas Hardy', email: 'thomashardy@mail.com', address: '89 Chiaroscuro Rd, Portland, USA', phone: '(171) 555-2222'},
         {id:uuidv4(), name: 'Dominique Perrier', email: 'dominiqueperrier@mail.com', address: 'Obere Str. 57, Berlin, Germany', phone: '(313) 555-5735'},
         {id:uuidv4(), name: 'Maria Anders', email: 'mariaanders@mail.com', address: '25, rue Lauriston, Paris, France', phone: '(503) 555-9931'},
@@ -14,22 +13,23 @@ const EmployeeContextProvider = (props) => {
         {id:uuidv4(), name: 'Martin Blank', email: 'martinblank@mail.com', address: 'Via Monte Bianco 34, Turin, Italy', phone: '(480) 631-2097'}
 ])
 
-      const addEmployee = (name, email, adress, phone) => {
-        setEmployees([
-          ...employees,
-          { id: uuidv4(), name, email, adress, phone },
-        ]);
-      };
-      
-      const deleteEmployee = (id) => {
-        setEmployees(employees.filter(employee => employee.id !== id))
-      }
+    //const sortedEmployees = employees.sort((a,b) => (a.name < b.name ? -1 : 1 ));
+    const sortedEmployees = employees.sort((a,b) => (a.name < b.name ? -1 : 1 ));
 
-      const updateEmployee = (id,updatedEmployee) => {
-            setEmployees(employees.map((employee)=> employee.id===id ? updatedEmployee:employee))
-      }
+    const addEmployee = (name, email, address, phone) => {
+        setEmployees([...employees, {id:uuidv4(), name, email, address, phone}])
+    }
+
+    const deleteEmployee = (id) => {
+        setEmployees(employees.filter(employee => employee.id !==id))
+    }
+
+    const updateEmployee = (id, updatedEmployee) => {
+        setEmployees(employees.map((employee) => (employee.id === id ? updatedEmployee : employee)))
+    }
+
     return (
-        <EmployeeContext.Provider value={{employees,addEmployee,deleteEmployee,updateEmployee}}>
+        <EmployeeContext.Provider value={{sortedEmployees, addEmployee, deleteEmployee, updateEmployee}}>
             {props.children}
         </EmployeeContext.Provider>
     )
