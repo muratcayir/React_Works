fbe527e5338368350e0dd3925df6a28d3683bf56
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-08-09 12:21:40 +0300

    Tooltip

--- ReactHooks/manage-app/src/components/Employee.js
+++ ReactHooks/manage-app/src/components/Employee.js
@@ -1,5 +1,5 @@
 import {EmployeeContext} from '../contexts/EmployeeContext'
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Modal ,OverlayTrigger,Tooltip} from 'react-bootstrap';
 import { useContext ,useState,useEffect} from 'react';
 import EditForm from './EditForm';
 
@@ -25,14 +25,30 @@ useEffect(() => {
           <td>{employee.adress}</td>
           <td>{employee.phone}</td>
           <td>
-          <button  onClick={handleShow}className="btn text-warning btn-act" data-toggle="modal">
-              <i className="material-icons" data-toggle="tooltip" title="Edit"> &#xE254;</i>
-            </button>
-            <button  onClick={()=> deleteEmployee(employee.id)} className="btn text-danger btn-act" data-toggle="modal">
-              <i className="material-icons" data-toggle="tooltip"  title="Delete">&#xE872;</i>
-            </button>
-          </td>   
 
+            <OverlayTrigger
+             overlay={
+               <Tooltip id={`tooltip-top`}>
+                 Edit
+               </Tooltip>
+             }>
+              <button  onClick={handleShow}className="btn text-warning btn-act" data-toggle="modal">
+              <i className="material-icons"> &#xE254;</i>
+            </button>
+            </OverlayTrigger>
+
+
+            <OverlayTrigger
+             overlay={
+               <Tooltip id={`tooltip-top`}>
+                Delete
+               </Tooltip>
+             }>
+              <button  onClick={()=> deleteEmployee(employee.id)} className="btn text-danger btn-act" data-toggle="modal">
+              <i className="material-icons" >&#xE872;</i>
+            </button>
+            </OverlayTrigger>  
+          </td>   
           <Modal show={show} onHide={handleClose}>
             <Modal.Header className="modal-header" closeButton>
                 <Modal.Title>
