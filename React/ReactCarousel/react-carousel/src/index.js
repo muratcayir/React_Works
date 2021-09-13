bcd7887403aa56d22281eab32d1c0a21eb59b872
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-09-13 16:17:56 +0300

    file is edited

--- React/ReactCarousel/react-carousel/src/index.js
+++ React/ReactCarousel/react-carousel/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './App';
-import Signup from "../src/components/Signup"
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-     <Signup/>
+     <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
